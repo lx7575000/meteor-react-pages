56b62f05941ea46f3612584597ff0e3c0f0dfbc0
lx7575000 <liu_narcisus@163.com>
2016-01-11 22:57:55 +0800

    添加 React-Router 、 sass相关支持

---
+++ s3-meteor/client/LogIn.jsx
@@ -0,0 +1,9 @@
+LogIn = React.createClass({
+	render(){
+		return (
+			<div className='login'>
+				this is log in pages ...
+			</div>
+		)
+	}
+});
---
+++ s3-meteor/client/SignUp.jsx
@@ -0,0 +1,9 @@
+SignUp = React.createClass({
+	render(){
+		return (
+			<div className='signup'>
+				this is sign up pages ...
+			</div>
+		)
+	}
+});
